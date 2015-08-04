f5751a1bf781c4a0a51aae4a40ce34c550d6eaa4
kianalavi <kian.alavi@gmail.com>
2015-08-03 17:34:40 -0700

    update to left-menu function

--- js/music.js
+++ js/music.js
@@ -2,6 +2,13 @@
 // Note utilizing soundcloud api to stream tracks
 var music = [
   {
+  tape: 'hnqo',
+  artist: 'HNQO',
+  artistlink: '<a href="https://soundcloud.com/hnqo">HNQO',
+  track_title: 'Whaaat!?',
+  track_url: 'https://api.soundcloud.com/tracks/58011123/stream?client_id=2a31bc8a1e12f2557a55ee962ab41670'
+  },
+  {
     tape: 'mcslee',
     artist: 'Mark Slee',
     artistlink: '<a href="https://soundcloud.com/mcslee">Mark Slee',
@@ -16,10 +23,25 @@ var music = [
     track_url: 'https://api.soundcloud.com/tracks/61369876/stream?client_id=2a31bc8a1e12f2557a55ee962ab41670'
   },
   {
+    tape: 'racheltorro',
+    artist: 'Rachel Torro',
+    artistlink: '<a href="https://soundcloud.com/racheltorro">Rachel Torro',
+    track_title: 'Flux',
+    track_url: 'https://api.soundcloud.com/tracks/170897157/stream?client_id=2a31bc8a1e12f2557a55ee962ab41670'
+  },
+  {
+    tape: 'leeburridge',
+    artist: 'Lee Burridge',
+    artistlink: '<a href="https://soundcloud.com/leeburridge">Lee Burridge',
+    track_title: 'Lightning in a Bottle',
+    track_url: 'https://api.soundcloud.com/tracks/105633861/stream?client_id=2a31bc8a1e12f2557a55ee962ab41670'
+  },
+  {
     tape: 'guygerber',
     artist: 'Guy Gerber',
     artistlink: '<a href="https://soundcloud.com/guygerber">Guy Gerber',
     track_title: 'Disco Knights',
     track_url: 'https://api.soundcloud.com/tracks/159717491/stream?client_id=2a31bc8a1e12f2557a55ee962ab41670'
   }
+
 ]
